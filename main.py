a6ccba76856502ec481cfff88cd629522617a5c7
mjsandagi <mjsandagi@users.noreply.github.com>
2024-11-20 01:14:15 +0000

    Added function to draw list bars to the canvas

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ class DrawInformation:
     GREY = (128, 128, 128)
     BACKGROUND_COLOUR = WHITE
     
+    GREYS = [ # GRADIENTS
+        GREY,
+        (160, 160, 160),
+        (192, 192, 192)
+    ]
+
     SIDE_PAD = 100
     TOP_PAD = 150
 
@@ -23,13 +29,29 @@ class DrawInformation:
 
     def set_list(self, lst):
         self.lst = lst
-        self.min_value = min(lst)
+        self.min_val = min(lst)
         self.max_val = max(lst)
 
         self.block_width = round((self.width - self.SIDE_PAD) / len(lst))
-        self.block_height = round((self.height - self.TOP_PAD) / (self.max_val - self.min_value))  
+        self.block_height = round((self.height - self.TOP_PAD) / (self.max_val - self.min_val))  
         self.start_x = self.SIDE_PAD // 2
 
+
+def draw(draw_info):
+    draw_info.window.fill(draw_info.BACKGROUND_COLOUR)
+    draw_list(draw_info)
+    pygame.display.update()
+
+
+def draw_list(draw_info):
+    lst = draw_info.lst
+    for i, val in enumerate(lst):
+        x = draw_info.start_x + i * draw_info.block_width
+        y = draw_info.height - (val - draw_info.min_val) * draw_info.block_height
+
+        colour = draw_info.GREYS[i % 3]
+
+        pygame.draw.rect(draw_info.window, colour, (x, y, draw_info.block_width, draw_info.height) )
 
 def generate_starting_list(n, min_val, max_val):
     lst = []
@@ -38,7 +60,6 @@ def generate_starting_list(n, min_val, max_val):
         lst.append(val)
 
     return lst
-
 
 def main():
     run = True
@@ -52,7 +73,8 @@ def main():
 
     while run:
         clock.tick(60)
-        pygame.display.update()
+        draw(draw_info)
+        # pygame.display.update()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
